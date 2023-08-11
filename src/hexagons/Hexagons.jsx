9f4d67c745526c5d2cb4857ba15abc67901a61ee
Youcef <youcef.ettaieb@gmail.com>
2023-08-11 11:53:26 +0200

    Add clouds

--- src/hexagons/Hexagons.jsx
+++ src/hexagons/Hexagons.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useRef, useState, useReducer } from "react";
+import React, { useEffect, useMemo, useState, useReducer } from "react";
 import * as THREE from "three";
 import "../styles/style.css";
 
 import * as BufferGeometryUtils from "three/examples/jsm/utils/BufferGeometryUtils.js";
 import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@3.0.0";
 
-import { SIDE } from "../App";
-
 import reducer, { SET_HEGAGONS, SET_SIZE } from "./reducer";
 import HexagonsMeshes from "../hexagons-meshes/HexagonsMeshes";
+import { useEnvironment } from "@react-three/drei";
 
 export const MAX_HEIGHT = 10;
 
@@ -29,6 +28,31 @@ const tilePosition = (tileX, tileY) => {
   );
 };
 
+const rock = (height, position) => {
+  const px = Math.random() * 0.4;
+  const pz = Math.random() * 0.4;
+
+  const geo = new THREE.SphereGeometry(Math.random() * 0.3 + 0.1, 7, 7);
+  geo.translate(position.x + px, height, position.y + pz);
+
+  return geo;
+};
+
+const tree = (height, position) => {
+  const treeHeight = Math.random() * 1 + 1.25;
+
+  const geo = new THREE.CylinderGeometry(0, 1.5, treeHeight, 3);
+  geo.translate(position.x, height + treeHeight * 0 + 1, position.y);
+
+  const geo2 = new THREE.CylinderGeometry(0, 1.15, treeHeight, 3);
+  geo2.translate(position.x, height + treeHeight * 0.6 + 1, position.y);
+
+  const geo3 = new THREE.CylinderGeometry(0, 0.8, treeHeight, 3);
+  geo3.translate(position.x, height + treeHeight * 1.25 + 1, position.y);
+
+  return BufferGeometryUtils.mergeGeometries([geo, geo2, geo3]);
+};
+
 const Hexagons = React.memo(({ i, j }) => {
   const initialState = {
     hexagons: {
@@ -41,6 +65,8 @@ const Hexagons = React.memo(({ i, j }) => {
     size: 1,
   };
 
+  const envMap = useEnvironment({ files: "/textures/envmap.hdr" });
+
   const [state, dispatch] = useReducer(reducer, initialState);
   const { hexagons, size } = state;
 
@@ -49,6 +75,43 @@ const Hexagons = React.memo(({ i, j }) => {
   let grass = HEXAGON;
   let sand = HEXAGON;
   let dirt2 = HEXAGON;
+
+  const clouds = useMemo(() => {
+    let geo = new THREE.SphereGeometry(0, 0, 0);
+    let count = Math.floor(Math.pow(Math.random(), 0.45) * 4);
+
+    const cloudMaterial = new THREE.MeshPhysicalMaterial({
+      envMap,
+      envMapIntensity: 0.75,
+      flatShading: true,
+    });
+
+    for (let i = 0; i < count; i++) {
+      const puff1 = new THREE.SphereGeometry(1.2, 7, 7);
+      const puff2 = new THREE.SphereGeometry(1.5, 7, 7);
+      const puff3 = new THREE.SphereGeometry(0.9, 7, 7);
+
+      puff1.translate(-1.85, 4, 0);
+      puff2.translate(0, 4, 0);
+      puff3.translate(1.85, 4, 0);
+
+      const cloudGeo = BufferGeometryUtils.mergeGeometries([
+        puff1,
+        puff2,
+        puff3,
+      ]);
+      cloudGeo.translate(
+        Math.random() * 20 - 10,
+        Math.random() * 7 + 7,
+        Math.random() * 20 - 10
+      );
+      cloudGeo.rotateY(Math.random() * Math.PI * 2);
+
+      geo = BufferGeometryUtils.mergeGeometries([geo, cloudGeo]);
+    }
+
+    return <mesh args={[geo]} material={cloudMaterial} />;
+  }, []);
 
   useEffect(() => {
     let counter = 0;
@@ -82,12 +145,33 @@ const Hexagons = React.memo(({ i, j }) => {
 
         if (height > STONE_HEIGHT) {
           stone = BufferGeometryUtils.mergeGeometries([stone, hexagonStone]);
+
+          if (Math.random() > STONE_HEIGHT) {
+            stone = BufferGeometryUtils.mergeGeometries([
+              stone,
+              rock(height, newPosition),
+            ]);
+          }
         } else if (height > DIRT_HEIGHT) {
           dirt = BufferGeometryUtils.mergeGeometries([dirt, hexagonStone]);
+
+          if (Math.random() > 0.8) {
+            grass = BufferGeometryUtils.mergeGeometries([
+              grass,
+              tree(height, newPosition),
+            ]);
+          }
         } else if (height > GRASS_HEIGHT) {
           grass = BufferGeometryUtils.mergeGeometries([grass, hexagonStone]);
         } else if (height > SAND_HEIGHT) {
           sand = BufferGeometryUtils.mergeGeometries([sand, hexagonStone]);
+
+          if (Math.random() > 0.8 && stone) {
+            stone = BufferGeometryUtils.mergeGeometries([
+              stone,
+              rock(height, newPosition),
+            ]);
+          }
         } else if (height > DIRT2_HEIGHT) {
           dirt2 = BufferGeometryUtils.mergeGeometries([dirt2, hexagonStone]);
         }
@@ -110,7 +194,12 @@ const Hexagons = React.memo(({ i, j }) => {
     });
   }, [i, j]);
 
-  return <HexagonsMeshes hexagons={hexagons} size={size} />;
+  return (
+    <>
+      {clouds}
+      <HexagonsMeshes hexagons={hexagons} size={size} />
+    </>
+  );
 });
 
 export default Hexagons;
