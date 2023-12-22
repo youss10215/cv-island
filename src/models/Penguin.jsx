eda657ef3576fe5d15b9d44c0a4785c55f90dd37
Youcef <youcef.ettaieb@gmail.com>
2023-12-22 15:35:44 +0100

    Change penguin model

--- src/models/Penguin.jsx
+++ src/models/Penguin.jsx
@@ -13,131 +13,44 @@ export const Penguin = (props) => {
   const { nodes } = useGraph(clone);
 
   useEffect(() => {
-    actions[names[0]].reset().fadeIn(0.5).play();
+    actions[names[1]].reset().fadeIn(0.5).play();
   }, [actions]);
 
   return (
-    <group ref={group} {...props} dispose={null} scale={1.5}>
+    <group ref={group} {...props} dispose={null} scale={0.0035}>
       <group name="Sketchfab_Scene">
         <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]}>
           <group
-            name="c0ba2d38caf34d1bb8e611c0f8f5f9a4fbx"
+            name="ee3fc61dae304d61bcd529d66822f459fbx"
             rotation={[Math.PI / 2, 0, 0]}
-            scale={0.01}
           >
             <group name="Object_2">
               <group name="RootNode">
-                <group name="Object_4">
-                  <primitive object={nodes._rootJoint} />
-                  <skinnedMesh
-                    name="Object_6"
-                    geometry={nodes.Object_6.geometry}
-                    material={materials.FurCoat}
-                    skeleton={nodes.Object_6.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_7"
-                    geometry={nodes.Object_7.geometry}
-                    material={materials.TransRump}
-                    skeleton={nodes.Object_7.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_8"
-                    geometry={nodes.Object_8.geometry}
-                    material={materials.TransTummy}
-                    skeleton={nodes.Object_8.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_9"
-                    geometry={nodes.Object_9.geometry}
-                    material={materials.TransLegs}
-                    skeleton={nodes.Object_9.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_10"
-                    geometry={nodes.Object_10.geometry}
-                    material={materials.FeathersUT}
-                    skeleton={nodes.Object_10.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_11"
-                    geometry={nodes.Object_11.geometry}
-                    material={materials.FeathersLT}
-                    skeleton={nodes.Object_11.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_12"
-                    geometry={nodes.Object_12.geometry}
-                    material={materials.TransThroat}
-                    skeleton={nodes.Object_12.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_13"
-                    geometry={nodes.Object_13.geometry}
-                    material={materials.TransNeck}
-                    skeleton={nodes.Object_13.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_14"
-                    geometry={nodes.Object_14.geometry}
-                    material={materials.Feet}
-                    skeleton={nodes.Object_14.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_15"
-                    geometry={nodes.Object_15.geometry}
-                    material={materials.Nails}
-                    skeleton={nodes.Object_15.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_16"
-                    geometry={nodes.Object_16.geometry}
-                    material={materials.Tongue}
-                    skeleton={nodes.Object_16.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_17"
-                    geometry={nodes.Object_17.geometry}
-                    material={materials.InnerMouth}
-                    skeleton={nodes.Object_17.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_18"
-                    geometry={nodes.Object_18.geometry}
-                    material={materials.CrestFluff}
-                    skeleton={nodes.Object_18.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_19"
-                    geometry={nodes.Object_19.geometry}
-                    material={materials.Beak}
-                    skeleton={nodes.Object_19.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_20"
-                    geometry={nodes.Object_20.geometry}
-                    material={materials.CrestMiddle}
-                    skeleton={nodes.Object_20.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_21"
-                    geometry={nodes.Object_21.geometry}
-                    material={materials.material}
-                    skeleton={nodes.Object_21.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_22"
-                    geometry={nodes.Object_22.geometry}
-                    material={materials.Pupil}
-                    skeleton={nodes.Object_22.skeleton}
-                  />
-                  <skinnedMesh
-                    name="Object_23"
-                    geometry={nodes.Object_23.geometry}
-                    material={materials.Iris}
-                    skeleton={nodes.Object_23.skeleton}
-                  />
+                <group
+                  name="PenguinArmature"
+                  rotation={[-Math.PI / 2, 0, 0]}
+                  scale={100}
+                >
+                  <group name="Object_5">
+                    <primitive object={nodes._rootJoint} />
+                    <skinnedMesh
+                      name="Object_28"
+                      geometry={nodes.Object_28.geometry}
+                      material={materials.Material}
+                      skeleton={nodes.Object_28.skeleton}
+                    />
+                    <group
+                      name="Object_27"
+                      rotation={[-Math.PI / 2, 0, 0]}
+                      scale={100}
+                    />
+                  </group>
                 </group>
+                <group
+                  name="Penguin"
+                  rotation={[-Math.PI / 2, 0, 0]}
+                  scale={100}
+                />
               </group>
             </group>
           </group>
@@ -146,7 +59,6 @@ export const Penguin = (props) => {
     </group>
   );
 };
-
 useGLTF.preload("/models/animals/penguin.glb");
 
 export default Penguin;
